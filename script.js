92f7730158a021013a37c750e47fb10edb140351
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-08-09 14:04:40 +0200

    Create raw layout Create menu items

---
+++ script.js
@@ -0,0 +1,23 @@
+const myLibrary = [
+
+    { title:"Gone with the Wind?", author: "Bree Zee", date: "2024", pages: 312 },
+    { title:"Byte Me?", author: "Chip L. O’Geek", date: "2023", pages: 254 },
+    { title: "Sofa, So Good", author: "Lou N. Geste", date: "December 2022", pages: 198 },
+    { title: "Java the Cup", author: "Mocha Latté", date: "July 2024", pages: 350 }
+
+];
+
+function Book(title, author, date, pages) {
+  this.title = title;
+  this.author = author;
+  this.date = date;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function displayBooks() {
+    
+}
